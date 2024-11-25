f2fd9cea7681b5a7856040f0c7a432a699c57bc1
songzhewu526 <songzhewu526@gmail.com>
2024-11-25 12:53:51 +0800

    修改處理股票代碼向量，存入同一份 google sheet

--- stock_data_to_googlesheets.R
+++ stock_data_to_googlesheets.R
@@ -11,11 +11,13 @@
 library(googlesheets4)
 
 symbol<- '2330.TW'
+
+symbol_list<- c('2330.TW', '2317.TW', '2454.TW')
 end_date <- Sys.Date()
 start_date <- end_date-365
 
-#先創建一個Google Sheet並貼上你的 Sheet ID
-sheet_id <- "XXXXXXXXXXXXX"
+#先創建一個Google Sheet並貼上你的 Sheet ID,  ex: https://docs.google.com/spreadsheets/d/xxxooo
+sheet_id <- "xxxooo"
 
 # 載入外部類別: security_data.R計算均線, KD, RSI數值
 source("stock_data_fetch.R")
@@ -43,21 +45,15 @@ update_my_sheet <- function(sheet_id, data, sheet_name = "工作表1", mode = "o
 }
 
 
-stock <- stock_data_fetch(symbol, start_date, end_date)
-
-# 如果需要保留時間索引作為欄位
-#my_df <- data.frame(
-#  date = index(stock),  # 將時間索引轉為欄位
-#  as.data.frame(stock)  # xts 的資料部分
-#)
-
-# xts 物件轉換時控制小數點
-my_df <- data.frame(
-  date = index(stock),
-  round(coredata(stock), digits = 2)
-)
-
-
-head(my_df)
-# 將 data frame 寫入以 symbol 為名稱的 Google Sheet:
-update_my_sheet(sheet_id=sheet_id, data=my_df, mode = "overwrite", sheet_name=symbol)
+for (symbol in symbol_list) {
+  
+  stock <- stock_data_fetch(symbol, start_date, end_date)
+  # xts 物件轉換時控制小數點
+  my_df <- data.frame(
+    date = index(stock),
+    round(coredata(stock), digits = 2)
+  )
+  # 將 data frame 寫入以 symbol 為名稱的 Google Sheet:
+  update_my_sheet(sheet_id=sheet_id, data=my_df, mode = "overwrite", sheet_name=symbol)
+  
+}
